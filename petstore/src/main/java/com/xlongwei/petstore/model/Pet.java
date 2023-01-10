ad8792bd0fd1386aff6b5895076e6aff038f361a
xlongwei <admin@xlongwei.com>
2023-01-10 18:31:27 +0800

    petstore

--- petstore/src/main/java/com/xlongwei/petstore/model/Pet.java
+++ petstore/src/main/java/com/xlongwei/petstore/model/Pet.java
@@ -1,6 +1,5 @@
 package com.xlongwei.petstore.model;
 
-import java.util.Arrays;
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
--- petstore/src/test/java/com/xlongwei/petstore/handler/TestServer.java
+++ petstore/src/test/java/com/xlongwei/petstore/handler/TestServer.java
@@ -7,14 +7,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import com.networknt.server.Server;
 import com.networknt.server.ServerConfig;
 
 public class TestServer extends ExternalResource {
     static final Logger logger = LoggerFactory.getLogger(TestServer.class);
 
     private static final AtomicInteger refCount = new AtomicInteger(0);
-    private static Server server;
 
     private static final TestServer instance  = new TestServer();
 
@@ -28,7 +26,7 @@ public class TestServer extends ExternalResource {
 
 
     public ServerConfig getServerConfig() {
-        return Server.config;
+        return Server.getServerConfig();
     }
 
     @Override
